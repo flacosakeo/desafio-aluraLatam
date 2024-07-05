17ff0e4cda7cdafbc0e40f4587d631d2688590c0
eduardo <flacosakeo@hotmail.com>
2024-07-05 18:22:38 -0300

    desafio alura

---
+++ app.js
@@ -0,0 +1,33 @@
+function encriptar (){
+    if (/[A-Z0-9]/.test(document.getElementById('input-texto').value)) {
+        alert('Por favor, ingrese solo letras minúsculas.');
+    }else{
+        var texto = document.querySelector("#input-texto").value;
+        var textoCifrado = texto.replace(/e/gi, "enter").replace(/i/gi, "imes").replace(/a/gi, "ai")
+                           .replace(/o/gi, "ober").replace(/u/gi, "ufat");
+        document.querySelector("#msg").value = textoCifrado;
+        document.querySelector("#input-texto").value;
+    }
+}
+    
+document.querySelector("#btn-encriptar").onclick = encriptar; 
+
+//////////////////////////////////////////////////////
+function desencriptar (){ 
+    var texto = document.querySelector("#input-texto").value; 
+    var textoCifrado = texto.replace(/enter/gi, "e").replace(/imes/gi, "i").replace(/ai/gi, "a")
+                       .replace(/ober/gi, "o").replace(/ufat/gi, "u");
+    document.querySelector("#msg").value = textoCifrado; 
+    document.querySelector("#input-texto").value;
+}
+
+document.querySelector("#btn-desencriptar").onclick = desencriptar; 
+
+////////////////////////////////////////////////////////
+function copiarPegar (){
+    document.getElementById('input-texto').value = '';
+    document.getElementById('input-texto').value = document.getElementById('msg').value;
+    document.getElementById('msg').value = '';
+}
+
+document.querySelector('#btn-copy').onclick = copiarPegar;
